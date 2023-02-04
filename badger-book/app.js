30988cf1dfef47f8af096592827c09e16262a797
Cole Nelson <ctnelson1997@gmail.com>
2023-02-04 10:35:04 -0600

    hw3

---
+++ badger-book/app.js
@@ -0,0 +1,29 @@
+/**
+ * TODO Your code goes below here!
+ * You may find the helper functions helpful.
+ */
+
+/**
+ * Given an array of students, generates HTML for all students
+ * using {@link buildStudentHtml}.
+ * 
+ * @param {*} studs array of students
+ * @returns html containing all students
+ */
+function buildStudentsHtml(studs) {
+	return studs.map(stud => buildStudentHtml(stud)).join("\n");
+}
+
+/**
+ * Given a student object, generates HTML. Use innerHtml to insert this
+ * into the DOM, we will talk about security considerations soon!
+ * 
+ * @param {*} stud 
+ * @returns 
+ */
+function buildStudentHtml(stud) {
+	let html = `<div>`;
+	html += `<h2>${stud.name.first} ${stud.name.last}</h2>`;
+	html += `</div>`
+	return html;
+}
